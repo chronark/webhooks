7b7ed9706752fc4b6d04b6abab4aea0842c3a431
chronark <dev@chronark.com>
2021-12-13 23:10:29 +0100

    use debounce window

--- test.bash
+++ test.bash
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 
-for VARIABLE in 1 2 3 4 5
+for n in 1 2 3 4 5
 do
+
 
 curl --request POST \
   --url https://webhooks-w74m.onrender.com/ \
-  --header 'Content-Type: application/json' \
   --header 'wh-debounce-window: 10' \
   --header 'wh-deduplication-id: abc' \
-  --header 'wh-target-url: https://hookb.in/QJ0pwLYaw0s8mNzzm267' \
-  --data '{
-	"data": "$VARIABLE"
-}'
-
+  --header 'wh-target-url: https://hookb.in/eKN2Yqy3wgulwQmmwRLm' \
+  --data "$n"
 done
 
 
