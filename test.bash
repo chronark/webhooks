bf5e0e1d39f8f9e69bd5adaa6844e49186f57c60
chronark <dev@chronark.com>
2021-12-13 23:10:59 +0100

    use debounce window

--- test.bash
+++ test.bash
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-
 for n in 1 2 3 4 5
 do
-
-
-curl --request POST \
-  --url https://webhooks-w74m.onrender.com/ \
-  --header 'wh-debounce-window: 10' \
-  --header 'wh-deduplication-id: abc' \
-  --header 'wh-target-url: https://hookb.in/eKN2Yqy3wgulwQmmwRLm' \
-  --data "$n"
+    curl --request POST \
+        --url https://webhooks-w74m.onrender.com/ \
+        --header 'wh-debounce-window: 10' \
+        --header 'wh-deduplication-id: abc' \
+        --header 'wh-target-url: https://hookb.in/eKN2Yqy3wgulwQmmwRLm' \
+          --data "$n"
 done
 
 
